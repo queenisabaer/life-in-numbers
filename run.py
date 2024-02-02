168426ebb75c1289267ac8326888044745867a10
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-02-02 11:16:58 +0100

    Initialize colorama

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True) # Reset the color and the style automatically
+
+
