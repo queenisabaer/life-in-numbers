ce6670472c48e8cb9e092ef328554034f174930b
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-02-04 17:13:56 +0100

    Create API settings and access google sheet

--- run.py
+++ run.py
@@ -1,8 +1,29 @@
-
-#import colorama for text editing
+# Import colorama for text editing
 import colorama
 from colorama import Fore, Back
 colorama.init(autoreset=True) # Reset the color and the style automatically
+
+'''
+Import gspread library:
+For this section I used the instructions 
+of the Code Institute love sandiwiches walkthrough
+'''
+import gspread
+# Import the credentials class from google oauth library 
+from google.oauth2.service_account import Credentials
+
+# Const to list the APIs that the  program should access in order to run.
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# Access the google sheet
+SHEET = GSPREAD_CLIENT.open('life-in-numbers')
 
 print(Fore.GREEN + r"""
  ____ ____ ____ ____ _________ ____ ____ ____ ____ _________ 
@@ -18,4 +39,4 @@ print("""
 Welcome!
       """)
  
- #centers the text in a line of 80 characters
+
